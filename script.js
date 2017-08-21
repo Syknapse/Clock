7ab64fcb657c58a4f1fda095c4f30a89e24b31a4
Syk Houdeib <syk.houdeib@gmail.com>
2017-08-21 19:27:55 +0200

    completed

---
+++ script.js
@@ -0,0 +1,27 @@
+const secondHand = document.querySelector('.second-hand');
+// minute hand
+const minsHand = document.querySelector('.min-hand');
+// hour hand
+const hourHand = document.querySelector('.hour-hand');
+
+// run a function every second
+function setDate(){
+	const now = new Date();
+
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+	minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+
+
+setInterval(setDate, 1000);
+setDate();
